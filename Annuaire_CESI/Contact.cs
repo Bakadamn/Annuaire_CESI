148928d849b7f714f88ae44f62899e12167068dd
Bastoboy <bakadamned@gmail.com>
2021-05-12 16:53:04 +0200

    Ajoutez des fichiers projet.

---
+++ Annuaire_CESI/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Annuaire_CESI
+{
+    [Table("Contact", Schema = "public")]
+    class Contact
+    {
+        [Key] // PRIMARY KEY
+        public int ClientID { get; set; }
+        [Required] //NOT NULL
+        public string Nom { get; set; }
+        [Required] //NOT NULL
+        public string Prenom { get; set; }
+        public string Telephone { get; set; }
+        public string Service { get; set; }
+        public DateTime DateEntree { get; set; }
+
+      
+    }
+}
